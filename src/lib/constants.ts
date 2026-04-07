933beb41060d88cea9138128a8ebff55e1ab74da
sarathyweb <sarathywebindia@gmail.com>
2026-04-07 06:34:14 +0000

    fix: update WhatsApp number to +919025589022

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const WHATSAPP_NUMBER = "14155238886";
+export const WHATSAPP_NUMBER = "919025589022";
 export const WHATSAPP_URL = `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent("Hi Charu!")}`;
 export const WHATSAPP_URL_WITH_UTM = `${WHATSAPP_URL}&utm_source=charuai.com&utm_medium=website&utm_campaign=launch`;
 export const CONTACT_EMAIL = "sarathy@sarathywebservices.com";
